e5dd2a369a3e3e4933412faffa8ecd27057d4c06
Darina Kukarina <60799367+DarinaKukarina42@users.noreply.github.com>
2021-01-25 22:41:23 -0500

    add scroll functions

---
+++ scroll.js
@@ -0,0 +1,10 @@
+window.onscroll = () => {
+    const nav = document.querySelector('#navbar');
+    if(this.scrollY <= 10) nav.className = ''; else nav.className = 'scroll';
+  };
+
+
+  AOS.init({
+    duration: 1200,
+  })
+
