368c46a73fd58a49f79cadb1a2a0d0f43734919a
Darina Kukarina <60799367+DarinaKukarina42@users.noreply.github.com>
2021-01-29 01:15:59 -0500

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,10 +1,10 @@
 window.onscroll = () => {
     const nav = document.querySelector('#navbar');
     if(this.scrollY <= 10) nav.className = ''; else nav.className = 'scroll';
-  };
+};
 
 
-  AOS.init({
+AOS.init({
     duration: 1200,
-  })
+})
 
